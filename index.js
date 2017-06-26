4524e73952f8b91147372848e3597be38d587b18
Thibault GERMAIN <thibaultgermain@MacBook-Pro-de-Thibault.local>
2017-06-26 16:58:02 +0200

    Progressing and backup before going into EJS

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
+//Varaiables
+
 var express = require('express');
+var ejs = require('ejs');
 var app = express(),
     port = 1337;
+
+//EJS 
+app.set('view engine', 'ejs');
+
+//Secure
 
 app.use('/assets/css', express.static(__dirname + '/public/css'));
 app.use('/assets/js', express.static(__dirname + '/public/js'));
 app.use('/assets/images', express.static(__dirname + '/public/images'));
 app.use('/assets/fonts', express.static(__dirname + '/public/fonts'));
+
+//Redirections
 
 app.get('/', function(req,res){
     console.log('REQ on /, with redirection ')
@@ -20,6 +30,11 @@ app.get('/lechef', function(req,res){
 app.get('/heritage', function(req,res){
     console.log('REQ on /heritage, with redirection ')
     res.sendFile(__dirname + '/views/heritage.html');
+})
+
+app.get('/lacave', function(req,res){
+    console.log('REQ on /heritage, with redirection ')
+    res.sendFile(__dirname + '/views/lacave.html');
 })
 
 app.listen(port, function(){
